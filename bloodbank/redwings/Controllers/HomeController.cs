ca214028e963bc65167788df18b94d90698b4431
_pnkj10 <pankajlokhande1981999@gmail.com>
2023-01-06 21:52:19 +0530

    add

---
+++ bloodbank/redwings/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using redwings.Models;
+
+namespace redwings.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+  public IActionResult home()
+    {
+        return View();
+    }
+    public IActionResult login()
+    {
+        return View();
+    }
+    public IActionResult aboutus()
+    {
+        return View();
+    }
+  
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
